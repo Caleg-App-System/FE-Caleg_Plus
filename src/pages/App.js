34093703ae98142b39f9543b97b4194717185eb0
setyarp <setyarizkypradana@gmail.com>
2023-01-31 15:55:37 +0700

    setup

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, store } from "../config";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Provider } from "react-redux";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Content } from "../../components";
+
+const Home = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+};
+
+export default Home;
