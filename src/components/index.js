0ff17aa6491152499df13b316e683a0da9710c28
setyarp <setyarizkypradana@gmail.com>
2023-02-07 12:57:54 +0700

    refator: directory user amangement

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Content from "./content";
 import Navbar from "./navbar";
 import RegisterComponent from "./register";
 import LoginComponent from "./login";
-import UserManagement from "./UserManagement";
+import UserManagement from "./Admin/UserManagement";
+import NavbarAdmin from "./Admin/navbarAdmin";
 
-export { Content, Navbar, LoginComponent, RegisterComponent, UserManagement };
+export { Content, Navbar, LoginComponent, RegisterComponent, UserManagement, NavbarAdmin };
