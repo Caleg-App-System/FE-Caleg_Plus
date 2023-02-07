9642d25d2dade3b02a5bdb135093ce345dabefa2
setyarp <setyarizkypradana@gmail.com>
2023-02-07 13:03:47 +0700

    fix: logout service

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -6,14 +6,14 @@ export const AuthService = {
     return response;
   },
 
-  login : async (data) => {
-    const response = await API.post('/auth/login', data);
+  login: async (data) => {
+    const response = await API.post("/auth/login", data);
     const Username = response.data.data.user.username;
     const Role = response.data.data.user.role;
     const id = response.data.data.user.id;
     const email = response.data.data.user.email;
     setHeadersAndStorage(response.data, Username, id, email, Role);
-    console.log(Username)
+    console.log(Username);
     // if (data.rememberMe === true) {
     //     setTimeout(() => {
     //         localStorage.removeItem('token');
@@ -28,34 +28,29 @@ export const AuthService = {
     //     , 86400000);
     // }
     console.log(response.data);
-    
+
     return response;
   },
 
-  logout : async () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('isLogged');
-    localStorage.removeItem('username');
-    localStorage.removeItem('id');
-    localStorage.removeItem('email');
-    localStorage.removeItem('role');
+  logout: async () => {
+    localStorage.clear();
     return;
   },
 
-  verifyAccount : async (data) => {
-    const response = await API.put('/verify', data);
+  verifyAccount: async (data) => {
+    const response = await API.put("/verify", data);
     return response;
   },
 };
 
-const setHeadersAndStorage = ({ user, token}, Username, id, email, Role,) => {
-  API.defaults.headers['Authorization'] = `Bearer ${token}`;
-  localStorage.setItem('username', JSON.stringify(Username));
-  localStorage.setItem('id', JSON.stringify(id));
-  localStorage.setItem('token', token);
-  localStorage.setItem('isLogged',true);
-  localStorage.setItem('role', JSON.stringify(Role));
+const setHeadersAndStorage = ({ user, token }, Username, id, email, Role) => {
+  API.defaults.headers["Authorization"] = `Bearer ${token}`;
+  localStorage.setItem("username", JSON.stringify(Username));
+  localStorage.setItem("id", JSON.stringify(id));
+  localStorage.setItem("token", token);
+  localStorage.setItem("isLogged", true);
+  localStorage.setItem("role", JSON.stringify(Role));
   if (email) {
-      localStorage.setItem('email', JSON.stringify(email));
+    localStorage.setItem("email", JSON.stringify(email));
   }
-}
+};
