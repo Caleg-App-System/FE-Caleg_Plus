d25d6c73d44a3557916fd7df4e1731dde410b8e2
setyarp <setyarizkypradana@gmail.com>
2023-02-07 13:00:23 +0700

    add: configure protected routes

--- src/config/Routes/Protected.js
+++ src/config/Routes/Protected.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { AuthService } from "../../services/authServices";
 
 function Protected({ children }) {
   // Navigate
@@ -10,6 +9,7 @@ function Protected({ children }) {
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
 
 useEffect(() => {
+  console.log(role)
     if (role !== "ADMIN" || !isLoggedIn) {
         navigate("/");
     }
