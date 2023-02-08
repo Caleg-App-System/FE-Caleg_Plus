ff5701dddea653f84ca8ec8d076343efe68633d8
setyarp <setyarizkypradana@gmail.com>
2023-02-08 07:13:36 +0700

    update: table using react data table

--- src/components/Admin/UserManagement/index.js
+++ src/components/Admin/UserManagement/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./usermanagement.css";
 import { UsersService } from "../../../services/usersServices";
+import DataTable from "react-data-table-component";
 
 const UserManagement = () => {
   const [update, setUpdate] = useState(false);
@@ -17,59 +18,61 @@ const UserManagement = () => {
     await UsersService.approval(id);
     setUpdate(!update);
   };
-
+  const columns = [
+    {
+      name: "NO",
+      cell: (row, index) => index + 1,
+      width: "80px", //RDT provides index by default
+    },
+    {
+      name: "Username",
+      selector: (row) => row.username,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+      sortable: true,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Role",
+      selector: (row) => row.role,
+      sortable: true,
+    },
+    {
+      name: "Status Akun",
+      selector: (row) => (row.is_verified_account === true ? "Terverifikasi" : "Belum Verifikasi"),
+      sortable: true,
+    },
+    {
+      name: "Status Approval",
+      selector: (row) => (row.is_verified_role === true ? "Approve" : "Belum Approve"),
+      sortable: true,
+    },
+    {
+      name: "Aksi",
+      cell: (row) => (
+        <button className="btn btn-primary" onClick={() => approvalHandler(row.id)}>
+          Acc
+        </button>
+      ),
+    },
+  ];
 
   return (
-    <>
-      <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 className="h2">Users</h1>
-          <div className="btn-toolbar mb-2 mb-md-0"></div>
-        </div>
-        <div className="table-responsive">
-          <table className="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th scope="col">NO</th>
-                <th scope="col">Username</th>
-                <th scope="col">Email</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Role</th>
-                <th scope="col">Status Account</th>
-                <th scope="col">Approved Account</th>
-                <th scope="col">Approval</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((users, index) => {
-                return (
-                  <>
-                    <tr>
-                      <td>{index + 1}</td>
-                      <td>{users.username}</td>
-                      <td>{users.email}</td>
-                      <td>{users.name}</td>
-                      <td>{users.role}</td>
-                      <td>{users.is_verified_account === true ? "Actived" : "Not Active"}</td>
-                      <td>{users.is_verified_role === true ? "Approved" : "Not Approved"}</td>
-                      {/* MODAL */}
-                      {/* <!-- Button trigger modal --> */}
-                      <td>
-                        <button className="btn btn-primary" onClick={() => approvalHandler(users.id)}>
-                          Acc
-                        </button>
-                        {/* <p className='p-4 text-danger' onClick={approvalHandler}>Read All</p> */}
-                      </td>
-                    </tr>
-                  </>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      </main>
-    </>
-  );
+  <>
+  <div className="content-election mx-5 my-5 px-2 py-2">
+    <div className="table-election text-center">
+      <DataTable title="Users" columns={columns} data={users} pagination />
+    </div>
+  </div>
+  </>
+  )
 };
 
 export default UserManagement;
