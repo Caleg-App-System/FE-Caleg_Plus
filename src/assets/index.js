cf1e933c757a909cdb9eafd38796ba22cf2316d7
setyarp <setyarizkypradana@gmail.com>
2023-02-12 16:36:14 +0700

    add: asset image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,6 @@
 import Logo from "./icons/logo.svg";
+import LogoBlack from "./image/LogoCPlus.png"
+import LogoWhite from "./image/LogoNew.png"
+import PdfLogo from "./image/pdf.png"
 
-export { Logo };
+export { Logo, LogoBlack, LogoWhite, PdfLogo };
