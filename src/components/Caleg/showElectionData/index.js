e7dc167c32dd478c59b4ed34cb8189ae6d1817ad
setyarp <setyarizkypradana@gmail.com>
2023-02-17 10:21:57 +0700

    update: displat election data

--- src/components/Caleg/showElectionData/index.js
+++ src/components/Caleg/showElectionData/index.js
@@ -33,73 +33,104 @@ const columns = [
   },
   {
     name: "Provinsi",
-    selector: (row) => row.tps.desa.kecamatan.kabupaten.provinsi.name,
+    selector: (row) => row.provinsi,
     sortable: true,
   },
   {
     name: "Kabupaten",
-    selector: (row) => row.tps.desa.kecamatan.kabupaten.name,
+    selector: (row) => row.kabupaten,
     sortable: true,
   },
   {
     name: "Kecamatan",
-    selector: (row) => row.tps.desa.kecamatan.name,
+    selector: (row) => row.kecamatan,
     sortable: true,
   },
   {
     name: "Desa",
-    selector: (row) => row.tps.desa.name,
+    selector: (row) => row.desa,
     sortable: true,
   },
   {
     name: "TPS",
-    selector: (row) => row.tps.name,
+    selector: (row) => row.tps,
     sortable: true,
     width: "150px",
   },
   {
     name: "DPT",
-    // selector: (row) => row.dpt,
+    selector: (row) => row.TotalDpt,
     sortable: true,
-    width: "100px",
+    width: "70px",
+    conditionalCellStyles: [
+      {
+        when: (row) => row.TotalDpt === undefined,
+        style: {
+          backgroundColor: "#d3d3d3",
+        },
+      },
+    ],
   },
   {
     name: "Nama Caleg",
-    selector: (row) => row.caleg.name,
+    selector: (row) => row.caleg,
     sortable: true,
   },
   {
     name: "Nama Partai",
-    selector: (row) => row.caleg.political_party.name,
+    selector: (row) => row.partai,
     sortable: true,
   },
   {
     name: "Suara",
-    // selector: (row) => row.totalVote,
+    selector: (row) => row.TotalVote,
     sortable: true,
-    width: "100px",
+    conditionalCellStyles: [
+      {
+        when: (row) => row.TotalVote === undefined,
+        style: {
+          backgroundColor: "#d3d3d3",
+        },
+      },
+    ],
   },
 ];
 
-
-
 const ShowElectionData = () => {
-  const [update, setUpdate] = useState(false);
-  const [election, setElection] = React.useState([]);
+  const [election, setElection] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value);
+  };
 
   useEffect(() => {
     VoteService.getAllVote().then((res) => {
-      setElection(res.data.data.result);
+      setElection(res.data.data.arr);
     });
-  }, [update]);
+  }, []);
 
-  
-  
   return (
     <>
       <div className="content-election mx-5 my-5 px-2 py-2">
         <div className="table-election text-center">
-          <DataTable title="Perolehan Suara 2019" columns={columns} data={election} customStyles={customStyles} pagination />
+          <DataTable
+            title="Perolehan Suara 2019"
+            columns={columns}
+            data={election.filter((row) => {
+              const values = Object.values(row).join(" ").toLowerCase();
+              return values.includes(searchValue.toLowerCase());
+            })}
+            subHeader
+            subHeaderComponent={
+              <div className="box-filter">
+                Search
+                <input className="input-search ms-2" type="search" placeholder="Cari... " value={searchValue} onChange={handleSearch} />
+              </div>
+            }
+            customStyles={customStyles}
+            pagination
+          />
         </div>
       </div>
     </>
