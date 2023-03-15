d0191cf23e5a9bbbf410d674fec6f7ad40937bbd
setyarp <setyarizkypradana@gmail.com>
2023-03-15 17:16:58 +0700

    update: responsive verification page

--- src/components/successVerification/index.js
+++ src/components/successVerification/index.js
@@ -6,20 +6,20 @@ import { useDispatch } from "react-redux";
 import { verifyAccountActions } from "../../config/redux/actions/authActions";
 
 const SuccessVerification = (props) => {
-const history = useNavigate();
-const dispatch = useDispatch();
+  const history = useNavigate();
+  const dispatch = useDispatch();
 
   if (props.tokenVerify) {
-    const token={token :props.tokenVerify};
+    const token = { token: props.tokenVerify };
     dispatch(verifyAccountActions(token, history));
     // console.log(token);
-}
+  }
 
   return (
     <div className="box-verif mx-auto my-5 px-5 py-5 text-center text-white">
       <h1 className="mb-4">Success Verification</h1>
-      <Check2Circle size={200} color={"#56f63d"}/>
-      <h4 className="mt-4">Silahkan kontak admin agar melakukan approve akun anda supaya bisa digunakan untuk login ke aplikasi <b>Caleg Plus</b></h4>
+      <Check2Circle size={200} color={"#50e333"} />
+      <h4 className="mt-4">Silahkan kontak admin untuk info lebih lanjut agar akun anda bisa login ke aplikasi <b>Caleg Plus</b></h4>
     </div>
   );
 }
