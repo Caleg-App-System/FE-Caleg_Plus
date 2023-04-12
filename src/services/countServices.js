fe9fb7ac39b64b3c8264107a8927b9ce96a81cba
setyarp <setyarizkypradana@gmail.com>
2023-04-12 22:30:25 +0700

    update dashboard

---
+++ src/services/countServices.js
@@ -0,0 +1,12 @@
+import API from "./api";
+
+export const CountServices = {
+  countDPT: async () => {
+    const response = await API.get("/dpt/count");
+    return response;
+  },
+  countDPP: async () => {
+    const response = await API.get("/dpp/count");
+    return response;
+  },
+};
